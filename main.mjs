9aa4ea6510dd3028c8e0efa85868b25667a971da
peoft <paulo.ostermann.filho@gmail.com>
2022-07-09 22:11:14 -0300

    exemplo de módulos

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Paulo", 48));
+console.log(mostraCidade("Paulo", "Porto Alegre"));
+console.log(mostraHobby("Paulo", "Corrida"));
